46c22a03c47222b2ffbbd00dee1f1df0311a868c
David Manouchehri <manouchehri@riseup.net>
2016-03-31 22:44:00 +0000

    Another problem down.

--- notes.m
+++ notes.m
@@ -3,6 +3,7 @@
 function [] = notes() 
 
 p1();
+p2();
 
 end
 
@@ -15,9 +16,31 @@ V = [5 10 15 20 17 ]; % Velocity (m/s)
 
 % will be using trapz since we have data points.
 
-Q = trapz(V, T);
+Q = trapz(V, T); % Pay attention to the order!
 
 fprintf('It traveled %f meters.\n', Q);
+
+end
+
+function [] = p2()
+
+x = [0  0.5  1    1.5  2  ];
+y = [2 10.1 15.7 17.1 11.7];
+
+% Estimate Z = f(x) dx from 0 to 2
+
+% ---- i. Via Trapezoidal Integration
+
+% h is the step size, 0.5 in this case.
+
+h = mean(diff(x));
+
+% a = 0.5/2 * (f(0) + 2*f(0.5) + 2*f(1) + 2*f(1.5) + 2*f(2)
+a = 0.5/2 * (y(1) + 2*y(2) + 2*y(3) + 2*y(4) + y(5));
+
+fprintf('The solution is %f.\n', a);
+
+% ---- ii. Via Simpson's 1/3 Rule
 
 end
 
